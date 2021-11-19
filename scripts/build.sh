47f2d3628eccf11cbb38a6867bc17aa466bcdc4c
Rui Fu <freelz@gmail.com>
2021-11-19 09:31:49 +0800

    add actions

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ex
+
+echo "Releasing Function Mesh Worker Service"
+
+version=${1#v}
+if [[ "x$version" == "x" ]]; then
+  echo "You need give a version number of the Function Mesh Worker Service"
+  exit 1
+fi
+
+# Create a direcotry to save assets
+ASSETS_DIR=release
+mkdir $ASSETS_DIR
+
+mvn clean install -DskipTests
+mv target/mesh-worker-service-*.nar  ./$ASSETS_DIR
+cp README.md ./$ASSETS_DIR/mesh-worker-service-readme.md
