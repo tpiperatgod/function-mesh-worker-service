6c84d960db36b25191215a26952c3db7b7e63f56
Rui Fu <freelz@gmail.com>
2021-11-23 15:51:49 +0800

    fix script

--- .ci/helm.sh
+++ .ci/helm.sh
@@ -306,6 +306,10 @@ function ci::verify_mesh_worker_service_pulsar_admin() {
    return 1
   fi
   RET=$(${KUBECTL} exec -n ${NAMESPACE} ${CLUSTER}-pulsar-broker-0 -- bin/pulsar-admin sinks create --name data-generator-sink --sink-type data-generator --inputs persistent://public/default/random-data-topic --custom-runtime-options '{"inputTypeClassName": "org.apache.pulsar.io.datagenerator.Person"}')
+  echo "${RET}"
+  if [[ "${RET}" != *"successfully"* ]]; then
+   return 1
+  fi
   ${KUBECTL} get pods -n ${NAMESPACE}
   sleep 120
   ${KUBECTL} get pods -n ${NAMESPACE} --field-selector=status.phase=Running | grep "data-generator-source"
