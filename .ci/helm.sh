c9b8932c286b26bfdf6875d2090544124a114dfd
Rui Fu <freelz@gmail.com>
2022-05-14 07:53:35 +0800

    fix CI

--- .ci/helm.sh
+++ .ci/helm.sh
@@ -613,8 +613,7 @@ function ci::verify_function_stats_api() {
       WC=$(${KUBECTL} get pods -n ${NAMESPACE} --field-selector=status.phase=Running | grep "api-java-fn" | wc -l)
     done
     sleep 20
-    ci:verify_exclamation_function "persistent://public/default/api-java-fn-input" "persistent://public/default/api-java-fn-output" "test-message" "test-message!" 30
-
+    ${KUBECTL} exec -n ${NAMESPACE} ${CLUSTER}-pulsar-broker-0 -- bin/pulsar-client produce -m "test-message" -n 10 persistent://public/default/api-java-fn-input"
     echo "java function test done"
     echo "get function stats"
     ${KUBECTL} exec -n ${NAMESPACE} ${CLUSTER}-pulsar-broker-0 -- bin/pulsar-admin functions stats --name api-java-fn
