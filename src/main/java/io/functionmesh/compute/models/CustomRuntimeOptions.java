988b84c4690e39cef0c87baf5f97e8f54fa9405a
Rui Fu <freeznet@users.noreply.github.com>
2022-11-30 02:43:13 +0800

    support passing `imageTag` via `custom runtime options` (#501)
    support passing imageTag

--- src/main/java/io/functionmesh/compute/models/CustomRuntimeOptions.java
+++ src/main/java/io/functionmesh/compute/models/CustomRuntimeOptions.java
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -44,4 +44,6 @@ public class CustomRuntimeOptions {
 
     @TrustedModeOnly
     private String logRotationPolicy;
+
+    private String runnerImageTag;
 }
